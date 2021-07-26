0bffd7b7002a2deda5a9b10a111631a768faea82
Belson BA <belsonba@VERMEER.PSLT30.local>
2021-07-26 20:09:58 +1000

    Second test

--- ppgen.py
+++ ppgen.py
@@ -1,6 +1,6 @@
 # File:      ppgen.py
 # Author:    Bruce Belson
-# Copyright: TODO
+# Copyright: See LICENSE in this repository
 # Version:   1.1
 # Date:      26-Jul-2021
 
@@ -12,14 +12,16 @@
 # Template list:
 # [ template-file, destination-folder, destonation-file, is_copied ] 
 
-# import os
 from string import Template
 from pathlib import Path
 from datetime import date
 import getpass
+import argparse
+from sys import version_info
 
 # Generate a single file from template and dictionary of substitutions
 def gen_file(in_path, out_path, dict, is_copied):
+  print('gen_file '+str(in_path)+' => '+str(out_path))
   src = in_path.read_text()
   if is_copied:
     result = src
@@ -54,9 +56,40 @@ def main(proj_name, libraries, debug_type):
     ['CMakeLists.txt', '.', 'CMakeLists.txt', False],
     ['pico_sdk_import.cmake', '.', 'pico_sdk_import.cmake', True] 
   ]
-  # TODO - append debug stuff
+  picoprobe_templates = [
+    ['picoprobe_launch.json', '.vscode', 'launch.json', False]
+  ]
+  # Append debug material to templates
+  if debug_type == 'picoprobe':
+    for i in picoprobe_templates: 
+      templates.append(i)
+  # Generate everything
   gen_project(templates, tpldir, pwdir / proj_name, dict)
 
-# TODO - add cmd line switches
+# Library list
+library_choices = ["pwm", "adc", "uart"]
+library_expanded = ["hardware_pwm", "hardware_adc", "hardware_uart"]
 
-main('Lab7', 'hardware_pwm hardware_pio', 'picoprobe')
+def expand_library_list(libs):
+  es = []
+  flat = [item for sublist in libs for item in sublist]
+  for c in flat:
+    if c in library_choices:
+      es.append(library_expanded[library_choices.index(c)])
+  return ' '.join(es)
+
+# Process cmd line switches
+pp_version_info = "1.1"
+ppgen_desc = f"Pico Project Generator. Version {pp_version_info}." 
+parser = argparse.ArgumentParser(description=ppgen_desc)
+parser.add_argument("project",help="name of project and of new folder containing project")
+# parser.add_argument("-v", "--version", help="show program version", action="store_true")
+parser.add_argument("-l", "--library", help="add support library to the make list", 
+  choices=library_choices, action="append", nargs='+')
+parser.add_argument("-d", "--debugger", help="add debugger support", 
+  choices=["picoprobe"])
+
+args = parser.parse_args()
+
+print(args)
+print(expand_library_list(args.library))
